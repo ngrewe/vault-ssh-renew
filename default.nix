851508856e7855551b817ce5611b7774d126605d
Niels Grewe <niels.grewe@halbordnung.de>
2020-07-22 20:55:00 +0200

    chore(build): Make poetry install deps on shell open

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 with import <nixpkgs> {}; {
   fEnv = stdenv.mkDerivation {
     name = "vault-ssh-renew";
-    buildInputs = [ stdenv
+    buildInputs = [
+      stdenv
       idea.pycharm-community
       python37Packages.poetry
       
     ];
+    shellHook = "poetry install";
   };
 }
 
